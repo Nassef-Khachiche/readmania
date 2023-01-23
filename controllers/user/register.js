dc7165634887fc46a209ad858fb586eb3143424e
Nassef Khachiche <nassefkhachiche1@gmail.com>
2023-01-23 19:58:33 +0100

    Added - MongoDB connection

--- controllers/user/register.js
+++ controllers/user/register.js
@@ -1,16 +1,48 @@
+const bcrypt = require('bcrypt');
+
 exports.register = async (req, res) => 
 {
+
     let {
-        fullname,
+        firstname,
+        middlename,
         lastname,
         email,
         password,
     } = req.body;
 
-    console.log(fullname);
+    // Hash password
+    password = await bcrypt.hash(req.body.password, 10);
 
-    res.status(200).json({
-        message: "test message",    
-    })
+    try {
+        const user = await prisma.users.create({
+            data: {
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                password: password,
+            }
+        })
 
+        res.status(200).json({
+            message: {
+                content: 'Register complete!'
+            }
+        });
+        return;
+
+    } catch (e) {
+
+        // P2002: Is there already a user with these unique fields? Then no further.
+        if (e.code === 'P2002') {
+
+            // The .code property can be accessed in a type-safe manner
+            res.status(400).json({
+                error: {
+                    content: "This user already exists"
+                }
+            })
+            return;
+        }
+    }
 }
